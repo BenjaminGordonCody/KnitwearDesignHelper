df1b48cacec51e33d3226d28e448b69b659f4740
BenjaminGordonCody <b.gordoncody@gmail.com>
2022-01-07 10:41:24 +0000

    Ammended notes to reflect default pattern repeat

--- GaugeObject.js
+++ GaugeObject.js
@@ -2,7 +2,7 @@ const getShapingSubsections = require("./shapingSubsections");
 class GaugeObject {
   constructor(sts, rows, horPattern = [2, 0], verPattern = [2, 0]) {
     // A pattern requiring "multiple of 6 + 2" would be described [6,2]
-    // The default values describe a pattern repeat of 1 st with no border,
+    // The default values describe a pattern repeat of 2 st with no border,
     // ie, no specified pattern stitch, stockinette, or garter
     // sts + rows describe how many of each must be knitted to create 10cm
     // of fabric
